e80a9d1bddf95f7ff5c27fb2c6635fad7f342912
Abhijeet Prasad <abhi.prasad16@gmail.com>
2024-06-20 01:21:04 +0530

    Footer with Links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
       <body>
         <Navbar />
         {children}
+        
       </body>
     </html>
   );
--- src/components/Recommendations.tsx
+++ src/components/Recommendations.tsx
@@ -4,7 +4,7 @@ const Recommendations = () => {
     return (
         <div className="container mx-auto flex flex-col flex-wrap justify-center items-center">
             <div className="m-4 text-2xl font-bold tracking-tight text-blue">recommendations</div>
-            <div className="text-4xl font-bold tracking-tight text-grey">what do my mentors say</div>
+            <div className="text-4xl font-bold tracking-tight text-grey p-4">what do my mentors say</div>
             <div className="m-5 p-4 bg-card border border-grey rounded-lg shadow">
                 <div className="mb-3 p-3 text-lg tracking-tighter font-normal">{recommendations.content}</div>
                 <div className="mb-3 p-3 text-lg text-blue font-semibold">~ {recommendations.author}</div>
